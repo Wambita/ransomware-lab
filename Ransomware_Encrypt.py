6a0c4eeed0c238c21f14bcf83da555905cec3b9c
Wambita <wambitafana@gmail.com>
2026-03-02 13:55:53 +0300

    Feat: import libariries and  functions  needed for encryption

---
+++ Ransomware_Encrypt.py
@@ -0,0 +1,9 @@
+import  os
+import hashlib
+
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+from cryptography.hazmat.primitives.kdf.scrypt import Scrypt
+from cryptography.hazmat.primitives import padding
+from cryptography.hazmat.backends import default_backend
+import platform
+
